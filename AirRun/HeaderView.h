59d5a2c061d5531a4cfc19b73a497eb6146753b8
ChenHao <info@mrchenhao.com>
2015-04-01 14:01:36 +0800

    add files

---
+++ AirRun/HeaderView.h
@@ -0,0 +1,13 @@
+//
+//  HeaderView.h
+//  AirRun
+//
+//  Created by ChenHao on 4/1/15.
+//  Copyright (c) 2015 AEXAIR. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HeaderView : UIView
+
+@end
---
+++ AirRun/HeaderView.m
@@ -0,0 +1,21 @@
+//
+//  HeaderView.m
+//  AirRun
+//
+//  Created by ChenHao on 4/1/15.
+//  Copyright (c) 2015 AEXAIR. All rights reserved.
+//
+
+#import "HeaderView.h"
+
+@implementation HeaderView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
