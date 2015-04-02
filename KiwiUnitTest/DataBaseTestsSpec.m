3010aa1e92b3ef2d20e423bbf527917fe66bfadf
ChenHao <info@mrchenhao.com>
2015-04-03 01:30:19 +0800

    add FMDB TO MANTLE

---
+++ KiwiUnitTest/DataBaseTestsSpec.m
@@ -0,0 +1,19 @@
+//
+//  DataBaseTestsSpec.m
+//  AirRun
+//
+//  Created by ChenHao on 4/3/15.
+//  Copyright 2015 AEXAIR. All rights reserved.
+//
+
+#import <Kiwi/Kiwi.h>
+#import "DataBaseTests.h"
+
+
+SPEC_BEGIN(DataBaseTestsSpec)
+
+describe(@"DataBaseTests", ^{
+
+});
+
+SPEC_END
---
+++ Pods/Target Support Files/Pods-AirRun-MTLFMDBAdapter/Pods-AirRun-MTLFMDBAdapter-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_AirRun_MTLFMDBAdapter : NSObject
+@end
+@implementation PodsDummy_Pods_AirRun_MTLFMDBAdapter
+@end
