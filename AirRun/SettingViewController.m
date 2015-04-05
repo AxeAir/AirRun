cc4c7718a3616c595457a14c1ae7d483ab86b5a3
ChenHao <info@mrchenhao.com>
2015-04-05 15:08:54 +0800

    update setting

--- AirRun/SettingViewController.m
+++ AirRun/SettingViewController.m
@@ -20,6 +20,7 @@
     
     [self setTitle:@"设置"];
     [[self.navigationController navigationBar] setBarTintColor:RGBCOLOR(85, 150, 204)];
+    self.tableView.separatorStyle = UITableViewCellSelectionStyleNone;
 }
 
 - (instancetype)init
@@ -33,12 +34,19 @@
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
-    return 1;
+    if(section == 0)
+    {
+        return 1;
+    }
+    if (section == 1) {
+        return 3;
+    }
+    return 3;
 }
 
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
 {
-    return 4;
+    return 3;
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
@@ -50,6 +58,60 @@
         cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:identifer];
     }
     
+    
+    UILabel *title = [[UILabel alloc] initWithFrame:CGRectMake(15, 0, 100, HEIGHT(cell))];
+    
+    [title setFont:[UIFont systemFontOfSize:14]];
+    [cell.contentView addSubview:title];
+    
+    switch (indexPath.section) {
+        case 0:
+            
+            break;
+            
+        case 1:
+            if (indexPath.row == 0) {
+                title.text = @"语音提示";
+                UIView *line = [[UIView alloc] initWithFrame:CGRectMake(15, HEIGHT(cell)-1, Main_Screen_Width-15, 0.5)];
+                [line setBackgroundColor:RGBCOLOR(235, 235, 235)];
+                [cell.contentView addSubview:line];
+            }
+            if (indexPath.row == 1) {
+                title.text = @"锻炼提醒";
+                UIView *line = [[UIView alloc] initWithFrame:CGRectMake(15, HEIGHT(cell)-1, Main_Screen_Width-15, 0.5)];
+                [line setBackgroundColor:RGBCOLOR(235, 235, 235)];
+                [cell.contentView addSubview:line];
+            }
+            if (indexPath.row == 2) {
+                title.text = @"同步数据";
+            }
+            
+            break;
+            
+        case 2:
+            if (indexPath.row == 0) {
+                title.text = @"意见反馈";
+                UIView *line = [[UIView alloc] initWithFrame:CGRectMake(15, HEIGHT(cell)-1, Main_Screen_Width-15, 0.5)];
+                [line setBackgroundColor:RGBCOLOR(235, 235, 235)];
+                [cell.contentView addSubview:line];
+            }
+            if (indexPath.row == 1) {
+                title.text = @"评价我们";
+                UIView *line = [[UIView alloc] initWithFrame:CGRectMake(15, HEIGHT(cell)-1, Main_Screen_Width-15, 0.5)];
+                [line setBackgroundColor:RGBCOLOR(235, 235, 235)];
+                [cell.contentView addSubview:line];
+            }
+            if (indexPath.row == 2) {
+                title.text = @"关于我们";
+            }
+            
+            
+            break;
+        default:
+            break;
+    }
+    
+    [cell setAccessoryType:UITableViewCellAccessoryDisclosureIndicator];
     return cell;
 }
 
