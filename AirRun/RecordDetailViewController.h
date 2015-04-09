35f79a469b5908fe5877d4f9174dd25322ee7dd1
JasonWu <794424709@qq.com>
2015-04-09 22:40:04 +0800

    完成查看记录详细信息页面

---
+++ AirRun/RecordDetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  RecordDetailViewController.h
+//  AirRun
+//
+//  Created by JasonWu on 4/9/15.
+//  Copyright (c) 2015 AEXAIR. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RecordDetailViewController : UIViewController
+
+@end
