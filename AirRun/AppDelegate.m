3dc29735dab22b92126a6906531254a6f3cf3cf1
JasonWu <794424709@qq.com>
2015-04-16 20:50:51 +0800

    更新后台模式，

--- AirRun/AppDelegate.m
+++ AirRun/AppDelegate.m
@@ -165,8 +165,12 @@
 - (void)applicationDidEnterBackground:(UIApplication *)application {
     // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
     // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-    [[BackgroundModelManager sharedInstance] openBackgroundModel];
-    [[UIApplication sharedApplication] beginBackgroundTaskWithExpirationHandler:nil];
+    
+    if ([RunManager shareInstance].runState == RunStateRunning) {
+        [[BackgroundModelManager sharedInstance] openBackgroundModel];
+        [[UIApplication sharedApplication] beginBackgroundTaskWithExpirationHandler:nil];
+    }
+    
 }
 
 - (void)applicationWillEnterForeground:(UIApplication *)application {
